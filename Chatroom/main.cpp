3d7432f5c7f4f48a70b2fc0f7d52629c74a6988e
Matthew Aris <124069912+MatthewAris@users.noreply.github.com>
2023-03-09 12:51:37 +0000

    Mixed Server and Client

---
+++ Chatroom/main.cpp
@@ -0,0 +1,55 @@
+#include "accepter.h"
+#include "queue.h"
+#include "list.h"
+#include "receiver.h"
+#include "util.h"
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <thread>
+
+int Server()
+{
+    Queue<std::string> queue;
+    List<std::shared_ptr<sf::TcpSocket>> sockets;
+    Accepter accepter(queue, sockets);
+    // TODO launch an accepter thread.
+    std::thread accepterThread(accepter);
+    accepterThread.join();
+    while(1)
+    {
+        std::string s = queue.pop();
+        std::cout << "Main read: \"" << s << "\"\n";
+        // TODO send to all in sockets. Be careful to synchronise.
+
+    }
+    return 0;
+}
+
+int Client()
+{
+    std::shared_ptr<sf::TcpSocket> socket = std::make_shared<sf::TcpSocket>();
+    // auto connect = [&] { return socket->connect(sf::IpAddress::getLocalAddress(), PORT); };
+    // net_run(connect, "connect");
+    sf::Socket::Status status = socket->connect(sf::IpAddress::getLocalAddress(), PORT);
+    if (status != sf::Socket::Done) {
+        return 1;
+    }
+    std::cout << "Connected\n";
+    Queue<std::string> queue;
+    // TODO launch a receiver thread to receive messages from the server.
+    std::string s = "";
+    while (1)
+    {
+        std::getline(std::cin, s);
+        std::cout << "Sending: \"" << s << "\"" << std::endl;
+        // TODO send messages to the server
+    }
+    return 0;
+}
+
+int main()
+{
+
+}
+
+
